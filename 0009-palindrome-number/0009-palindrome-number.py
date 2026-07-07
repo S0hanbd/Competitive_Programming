be0c993ea043c8fd74942f0ad4cddcce249cf01e
Sohanur Rahman Sohan <sohanbdtech@gmail.com>
2026-07-08 03:38:46 +0600

    Time: 1 ms (96.48%), Space: 12.5 MB (17.15%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,12 +1,6 @@
 class Solution(object):
     def isPalindrome(self, x):
-        if x < 0:
-            return False
-        num = []
-        x = str(x)
-        for i in range(len(x) // 2):
-            if x[i] != x[-i-1]:
-                return False
-        return True
+       x = str(x)
+       return x == x[::-1]
 
         
